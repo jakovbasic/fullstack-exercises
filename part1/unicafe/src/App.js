787ae33d01079d1cfc3fa064f1c07e8f147ce025
jakovbasic <jakov.basic@gmail.com>
2022-12-27 20:48:25 +0200

    exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,44 +16,30 @@ const Button = (props) => (
   </button>
 )
 
-const Stat = (props) => {
-  return(
+const StatisticLine = (props) => {
+  if(props.text === "positive") {
+    return (
+      <div>
+        {props.text} {props.value} %
+      </div>
+      )
+  } else return(
   <div>
-    {props.name} {props.stats}
+    {props.text} {props.value} 
   </div>
   )
 }
 
-const Average = (props) => {
-  const avg = (props.good - props.bad)/(props.good + props.neutral + props.bad)
-  return (
-    <div>
-      average {avg}
-    </div>
-  )
-}
-
-const Positive = (props) => {
-  const pos = (props.good)/(props.good + props.neutral + props.bad)
-  return (
-    <div>
-      positive {pos} %
-    </div>
-  )
-}
-
-const Statistics = (props) => {
+const Statistics = ({good, neutral, bad}) => {
+  const avg = (good - bad)/(good + neutral + bad)
+  const pos = (good)/(good + neutral + bad)
   return(
     <div>
-      <Stat stats = {props.good[0]} name = {props.good[1]} />
-      <Stat stats = {props.neutral[0]} name = {props.neutral[1]} />
-      <Stat stats = {props.bad[0]} name = {props.bad[1]} />
-      <Average good = {props.good[0]}
-               neutral = {props.neutral[0]}
-               bad = {props.bad[0]} />
-      <Positive good = {props.good[0]}
-                neutral = {props.neutral[0]}
-                bad = {props.bad[0]} />
+      <StatisticLine text="good" value = {good} />
+      <StatisticLine text="neutral" value = {neutral} />
+      <StatisticLine text="bad" value = {bad} />
+      <StatisticLine text="average" value = {avg}/>
+      <StatisticLine text="positive" value = {pos}/>
     </div>
   )
 }
@@ -64,9 +50,9 @@ if(props.good + props.neutral + props.bad === 0) {
 }
 else return (
   <div>
-    <Statistics good = {[props.good,"good"]} 
-                neutral = {[props.neutral,"neutral"]} 
-                bad = {[props.bad,"bad"]} />
+    <Statistics good = {props.good} 
+                neutral = {props.neutral} 
+                bad = {props.bad} />
   </div>
   )
 }

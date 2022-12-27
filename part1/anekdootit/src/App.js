245e947de0e4d4a3a38d9f1b8d79584cdd6484ff
jakovbasic <jakov.basic@gmail.com>
2022-12-27 22:41:10 +0200

    exercises 1.13-1.14

--- part1/anekdootit/src/App.js
+++ part1/anekdootit/src/App.js
@@ -1,12 +1,35 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  return(
+  <div>
+    <h1>{props.header}</h1>
+  </div>
+  )
+}
+
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
   </button>
 )
 
+const Anecdote = (props) => {
+  return(
+    <div>
+      {props.anecdote} has {props.votes} votes
+    </div>
+  )
+}
+
+const votes = new Uint8Array(7)
+
 const App = () => {
+  const header1 = "Anecdote of the day"
+  const header2 = "Anecdote with most votes"
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(0)
+
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -17,17 +40,30 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
 
-  const handleClick = () => {
+  const maxvotes = Math.max(...votes)
+  var i = 0
+  while(votes[i] < maxvotes) {
+    i++
+  }
+
+  const handleNext = () => {
     let number = Math.floor(Math.random() * anecdotes.length)
     setSelected(number)
   }
-   
-  const [selected, setSelected] = useState(0)
 
+  const handleVote = () => {
+    votes[selected] += 1
+    setVote(vote +votes[selected])
+  }
+   
   return (
     <div>
-      {anecdotes[selected]}
-      <Button handleClick = {handleClick} text = "next anecdote" />
+      <Header header = {header1} />
+      <Anecdote anecdote = {anecdotes[selected]} votes = {votes[selected]}/>
+      <Button handleClick = {handleVote} text = "vote" />
+      <Button handleClick = {handleNext} text = "next anecdote" />
+      <Header header = {header2} />
+      <Anecdote anecdote = {anecdotes[i]} votes = {maxvotes}/>
     </div>
   )
 }

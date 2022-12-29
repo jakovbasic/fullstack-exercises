d38bdfe50231fac0d6f2e68db4c3497c3657345b
jakovbasic <jakov.basic@gmail.com>
2022-12-29 11:59:52 +0200

    exercise 2.9

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 
+const Header = ({header}) => {
+  return(<div><h1>{header}</h1></div>
+  )}
+
 const Persons = (props) => {
   return(
     <div>
@@ -11,19 +15,26 @@ const Persons = (props) => {
 } 
 
 const App = () => {
+  const head1 = "Phonebook"
+  const head2 = "add a new"
+  const head3 = "Numbers"
+
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
+  const names = persons.map(person => person.name)
 
   const addName = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
 
-    const names = persons.map(person => person.name)
     if(names.includes(newName)) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
@@ -46,16 +57,27 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const filteredPersons = (newFilter === '')
+  ? persons : persons.filter(person => person.name.includes(newFilter))
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <Header header ={head1}/>
+      <form>
+        <div>filter shown with <input value = {newFilter} onChange={handleFilterChange}/></div>
+      </form>
+      <Header header ={head2}/>
       <form onSubmit={addName}>
         <div>name: <input value = {newName} onChange={handleNameChange}/></div>
         <div>number: <input value = {newNumber} onChange={handleNumberChange}/></div>
         <div><button type="submit">add</button></div>
       </form>
-      <h2>Numbers</h2>
-      <Persons show = {persons} />
+      <Header header ={head3}/>
+      <Persons show = {filteredPersons} />
     </div>
   )
 

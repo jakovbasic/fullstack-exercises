6c17e162bfe55cfc28bc390d21a5e324aefbc9f6
jakovbasic <jakov.basic@gmail.com>
2022-12-31 13:34:00 +0200

    exercise 2.15

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -70,6 +70,11 @@ const App = () => {
       setPersons(persons.concat(nameObject))
       setNewName('')
       setNewNumber('')
+      axios
+        .post('http://localhost:3001/persons', nameObject)
+        .then(response => {
+          console.log(response)
+        })
     }
   }
 

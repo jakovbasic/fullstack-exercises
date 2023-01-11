7c01889aca7ec8eb82eea7bacc32174ab3004255
jakovbasic <jakov.basic@gmail.com>
2023-01-11 20:33:16 +0200

    exercise 4.4

--- part4/blogilista/utils/list_helper.js
+++ part4/blogilista/utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+  const sum = blogs.map(blog => blog.likes)
+                          .reduce((partialSum, a) => partialSum + a, 0)
+  return sum
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }

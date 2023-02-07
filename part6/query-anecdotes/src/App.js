0571436d2a8d41e8c5ec1f97fa09b81f81359548
jakovbasic <jakov.basic@gmail.com>
2023-02-08 00:26:49 +0200

    6.20

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,3 +1,5 @@
+import { useQuery } from 'react-query'
+import { getAnecdotes } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
@@ -7,13 +9,18 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery('anecdotes', getAnecdotes,  {retry: 1})
+  console.log(result)
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
